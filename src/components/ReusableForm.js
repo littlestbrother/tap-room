eb217382009a4d55a99a918ed8e016604dcb19ec
Aaron Kauffman <aaron.christian.kauffman@gmail.com>
2022-01-15 20:24:04 -0800

    add styling

--- src/components/ReusableForm.js
+++ src/components/ReusableForm.js
@@ -6,13 +6,17 @@ function ReusableForm(props) {
     <React.Fragment>
       <form onSubmit={props.formSubmissionHandler}>
         <p>Name</p>
-        <input type="text" name="name" placeholder="Name" />
+        <input type="text" name="name" placeholder="Abc" />
+        <br/><br/>
         <p>Brand</p>
-        <input type="text" name="brand" placeholder="Brand" />
+        <input type="text" name="brand" placeholder="Abc" />
+        <br/><br/>
         <p>Price</p>
         <input type="number" name="price" placeholder="$10" min={0} />
+        <br/><br/>
         <p>Flavor</p>
-        <input type="text" name="flavor" placeholder="Flavor" />
+        <input type="text" name="flavor" placeholder="Abc" />
+        <br/><br/>
         <p>Quantity (pints)</p>
         <input
           type="number"
@@ -21,7 +25,7 @@ function ReusableForm(props) {
           min="1"
           max="124"
         />
-        <br /><br/>
+        <br/><br/>
         <button type="submit">{props.buttonText}</button>
       </form>
     </React.Fragment>
