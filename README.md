17c41b3d6d4663a71c54bda7e986773a01a49406
Aaron Kauffman <aaron.christian.kauffman@gmail.com>
2022-01-15 18:40:53 -0800

    add boilerplate code

---
+++ README.md
@@ -0,0 +1,3 @@
+Droopy likes your face
+
+if stuff doesn't run ```yarn start```
---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Header from "./Header";
+import DrinkControl from "./DrinkControl";
+function App() {
+  return (
+    <React.Fragment>
+    </React.Fragment>
+  );
+}
+
+export default App;
